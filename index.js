5783419b51b7bcd4886e209f0edf1a14b37fb867
Mobeen88 <mobeenali1238@gmail.com>
2025-10-15 21:35:41 +0100

    Added styling

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 var http = require("http");
+var style = require("fs");
 const port = 8000;
 
 http.createServer(function (req, res) {
-  //console.log(req);
-  res.writeHead(200, { "Content-Type": "text/html" });
-  const htmlContent = `
-  <!DOCTYPE html>
-  <html lang = "en">
-  <head>
-      <meta charset = "UTF-8">
-      <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
-      <title>My Page</title>
-  </head>
-  <body>
-      <h1>Welcome to My Web Page</h1>
-      <p> Hello World! My name is Mobeen! </p>
-  </body>
-  </html>
-  `;
-
-  res.end(htmlContent);
+  if(req.url == "/style.css"){
+    res.writeHead(200, { "Content-Type": "text/css" });
+    style.createReadStream("style.css").pipe(res);
+  }
+  else {
+    const htmlContent = `
+      <!DOCTYPE html> 
+      <html lang = "en">
+      <head>
+          <meta charset = "UTF-8">
+          <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
+          <title> My Page </title>
+          <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+          <h1>Welcome to My Web Page</h1>
+          <p> Hello World! Welcome to nothing so far... </p>
+      </body>
+      </html>
+    `;
+    res.end(htmlContent);
+  }
 }).listen(port, function () {
   console.log(`✅ Node server is running at http://localhost:${port}`);
 });
